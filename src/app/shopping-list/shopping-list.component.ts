494db0dfcfadec3f7e1f9b863f149487c0112ed1
Md. Abul Hasan Sajli <aniksajli@gmail.com>
2021-03-23 18:04:02 +0600

    created logging.service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from './ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,10 +10,12 @@ import {ShoppingListService} from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService,
+              private loggingService: LoggingService) { }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.loggingService.printLog('Hello from shopping list component');
   }
 
   // onIngredientAdded(ingredient: Ingredient): void {
